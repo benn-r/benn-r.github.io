0924b8185da4eedb5ea6b25c7224b11f868fd847
Ben Rosario <b.enn@icloud.com>
2022-09-26 15:51:03 -0700

    Added new button

--- scripts.js
+++ scripts.js
@@ -29,12 +29,12 @@
 //                           %&%&.                                                
 
 /**
- *  Logic for blue button / cat meow
+ *  Logic for blue button - cat meow
  */
 
 function soundMeow() {
   let meow = new Audio("./resources/cat-meow.wav");
-  meow.play()
+  meow.play();
 }
 
 const blueButton = document.getElementById("blueButton");
@@ -42,11 +42,11 @@ blueButton.addEventListener("click", soundMeow);
 
 
 /**
- *  Logic for red button
+ *  Logic for red button - gun fire
  */
 function soundGun() {
   let gun = new Audio("./resources/pp7_fire1.wav");
-  gun.play()
+  gun.play();
 }
 
 const redButton = document.getElementById("redButton");
@@ -54,11 +54,11 @@ redButton.addEventListener("click", soundGun);
 
 
 /**
- *  Logic for green button
+ *  Logic for green button - scream sound
  */
 function soundScream() {
   let scream = new Audio("./resources/wilhelm-scream.wav");
-  scream.play()
+  scream.play();
 }
 
 const greenButton = document.getElementById("greenButton");
@@ -66,12 +66,23 @@ greenButton.addEventListener("click", soundScream);
 
 
 /**
- *  Logic for yellow button
+ *  Logic for yellow button - horn noise
  */
 function soundHorn() {
   let horn = new Audio("./resources/horn.wav");
-  horn.play()
+  horn.play();
 }
 
-const yellowButton = document.getElementById("yellowButton")
-yellowButton.addEventListener("click", soundHorn)
+const yellowButton = document.getElementById("yellowButton");
+yellowButton.addEventListener("click", soundHorn);
+
+/**
+ *  Logic for black button - eerie noise
+ */
+function soundScary() {
+  let scary = new Audio("./resources/gasp.wav");
+  scary.play();
+}
+
+const blackButton = document.getElementById("blackButton");
+blackButton.addEventListener("click", soundScary);
